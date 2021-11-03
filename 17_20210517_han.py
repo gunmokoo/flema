b9da597ab9df47d0d05e0256f23e691263dae98c
gunmokoo <minju1348@gmail.com>
2021-11-04 00:15:20 +0900

    creating!

--- 17_20210517_han.py
+++ 17_20210517_han.py
@@ -12,17 +12,7 @@ pygame.display.set_caption("일조") # 타이틀 제목
 clock = pygame.time.Clock() #프레임 설정 *이거 나중에 필요함
 
 background = pygame.image.load("C:/PythonWorkspace/flema/img/display.jpg") # 배경 그림 불러오기
-
-setting = pygame.image.load("C:/PythonWorkspace/flema/img/setting.png") # 설정 그림 불러오기
-setting_size = setting.get_rect().size # 설정 이미지의 크기를 구해옴
-setting_w = setting_size[0] # 설정 가로 크기
-setting_h = setting_size[1] # 설정 세로 크기
-
-# setscreen = pygame.image.load("C:/PythonWorkspace/flema/img/설정화면.png") #설정화면 그림 불러오기
-# setscreen_size = setscreen.get_rect().size # 설정 이미지의 크기를 구해옴
-# setscreen_w = setscreen_size[0] # 설정화면 가로 크기
-# setscreen_h = setscreen_size[1] # 설정화면 세로 크기
-# setscreens = []
+background_opa = pygame.image.load("C:/PythonWorkspace/flema/img/display2.png") # 배경 그림 불러오기
 
 arrowr = pygame.image.load("C:/PythonWorkspace/flema/img/cursor_r.png") # 오른쪽 화살표 그림 불러오기
 aroowr_size = arrowr.get_rect().size # 오른쪽 화살표 이미지의 크기를 구해옴
@@ -33,6 +23,11 @@ arrowl = pygame.image.load("C:/PythonWorkspace/flema/img/cursor_l.png") # 왼쪽
 aroowl_size = arrowl.get_rect().size # 왼쪽 화살표 이미지의 크기를 구해옴
 arrowl_w = aroowl_size[0] # 왼쪽 화살표 가로 크기
 arrowl_h = aroowl_size[1] # 왼쪽 화살표 세로 크기
+
+yesno = pygame.image.load("C:/PythonWorkspace/flema/img/yesno.jpg") # 예쓰버튼 그림 불러오기
+yesno_size = yesno.get_rect().size # 설정 이미지의 크기를 구해옴
+yesno_w = yesno_size[0] # 설정 가로 크기
+yesno_h = yesno_size[1] # 설정 세로 크기
 
 yes_button = pygame.image.load("C:/PythonWorkspace/flema/img/예.png") # 예쓰버튼 그림 불러오기
 yes_button_size = yes_button.get_rect().size # 설정 이미지의 크기를 구해옴
@@ -49,7 +44,7 @@ game_bar_size = game_bar.get_rect().size # 바 이미지의 크기를 구해옴
 game_bar_w = game_bar_size[0] # 바 가로 크기
 game_bar_h = game_bar_size[1] # 바 세로 
 
-back_button = pygame.image.load("C:/PythonWorkspace/flema/img/이전.jpg") # 뒤로가기 그림 불러오기
+back_button = pygame.image.load("C:/PythonWorkspace/flema/img/이전.png") # 뒤로가기 그림 불러오기
 back_button_size = back_button.get_rect().size # 뒤로가기 이미지의 크기를 구해옴
 back_button_w = back_button_size[0] # 뒤로가기 가로 크기
 back_button_h = back_button_size[1] # 뒤로가기 세로
@@ -71,7 +66,6 @@ RIGHT = 3 #마우스 오른쪽 클릭
 
 back_button_screen = 0 # 뒤로가기 버튼
 yes_no = 0 # yes no 버튼 불러오기 위한 변수
-setting_screen = 0 # 설정화면을 불러오기 위한 변수
 game = 0 # 게임 화면을 불러오기 위한 변수
 
 start_ticks = pygame.time.get_ticks() # 게임 시작 시간 정보
@@ -80,15 +74,19 @@ game_font = pygame.font.Font(None, 40) # 폰트 객체 생성
 no_pull = 0
 note_one = 1
 yes_controll = 1
+fairy_condition = 0
 
 crash_square = pygame.image.load("C:\\Pythonworkspace\\flema\\img\\line.png") 
 crash_square_size = crash_square.get_rect().size
 crash_square_w = crash_square_size[0]
 crash_square_h = crash_square_size[1]
 
+fairy = pygame.image.load("C:/PythonWorkspace/flema/img/fairy.png") # 요정 그림 불러오기
+
 game_down_remove = -1 # 떨어지는 바 사라지게 하기위한 변수
 
 game_score = 0
+game_play = 0
 
 running = True # 게임이 진행중인지
 while running: # True 시 진행 False 시 종료
@@ -147,21 +145,19 @@ while running: # True 시 진행 False 시 종료
             if 515.5 + yn_album_h + ( yn_no_button_h /2) <= mousepos[0] <= 515.5 + (yn_album_h) + ( yn_no_button_h /2) + yn_no_button_w \
             and 277.5 + (yn_album_w/3) <=mousepos[1] <=  277.5+(yn_album_w/3) + yn_no_button_h: # 아니오 버튼 클릭 좌표 
                     yes_no = 0 # 변수를 0으로
+                    fairy_condition += 1
             if 515.5 + yn_album_h - ( yn_yes_button_h *2) <= mousepos[0] <= 515.5 + yn_album_h - ( yn_yes_button_h *2) + yn_yes_button_w \
             and 277.5 + (yn_album_w/3) <=mousepos[1] <=  277.5+(yn_album_w/3) + yn_yes_button_h: # 예 버튼 클릭 좌 
                     yes_no = 0 # 변수를 0으로
+                    fairy_condition = 0
                     game += 1 # 변수에 1을 더함
                     album_opacity = 1
                     start_ticks = pygame.time.get_ticks() # 게임 시작 시간 정보
                     pygame.mixer.music.load("C:/PythonWorkspace/flema/music{0}.mp3".format(album_num)) #음악 불러오기
                     pygame.mixer.music.play(0) #음악 재생
+                    pygame.mixer.music.set_volume(0.1)
+
                 
-            # if 1110 <= mousepos[0] <= 1110 + setscreen_w and 50 <= mousepos[1] <= 50 + setting_h: #설정 클릭 했을때
-            #     setting_screen += 1 #setting_screen 변수에 1을 저장
-            #     print(setting_screen);
-            #     if game >=1:
-            #         setting_screen = 0
-            #         back_button_screen = 0
                     
         if event.type == pygame.KEYDOWN: # 키보드 입력 이벤트 발생하려면 필요
             if event.key == pygame.K_LEFT: # 키보드 왼쪽버튼이 눌릴시에
@@ -275,31 +271,37 @@ while running: # True 시 진행 False 시 종료
     game_downs = [ [g[0], g[1] + game_down_speed] for g in game_downs] # x, y 값 갱신
     game_downs = [ [g[0], g[1]] for g in game_downs if g[1] < 703] # y 값이 바닥에 닿으면 사라짐
 
-    if setting_screen >= 1: # 변수가 1이상 일 때
-        setscreens = [ [g[0], g[1] + game_down_speed] for g in setscreens] # x, y 값 갱신
-        setscreens = [ [g[0], g[1]] for g in setscreens if g[1] < 720] # y 값이 바닥에 닿으면 사라짐
-
     def real_game_down(event): # 묻지마 나도 헷갈려
         if event == int(elapsed_time): # real_game_down(숫자) 숫자가 시간과 같을시 나오게
             game_down_x_pos = sample(game_down_x, 1) # x 값 랜덤
             game_down_y_pos = -100 # 떨어지는 바 y 값
             game_downs.append([game_down_x_pos, game_down_y_pos]) # x, y 값 갱신
+        if int(elapsed_time) == 79:
+            game = 0;
+            game_score = 0
+            pygame.mixer.music.stop() #노래 끄기
+            yes_no = 0
+            album_opacity = 0
+            game_play = 0
 
     if game_down_remove > -1:
         del game_downs[game_down_remove]
         game_down_remove = -1
 
     screen.blit(background, (0, 0)) # 0, 0 좌표에 배경 불러오기
-    screen.blit(setting, (1110, 50)) # 좌표에 설정 불러오기
     screen.blit(album,(515.5,277.5)) #앨범 불러오기
     if album_num != 3 and album_opacity ==0:
         screen.blit(arrowr, (1030, 250)) # 좌표에 오른쪽 화살표 불러오기
     if album_num != 1 and album_opacity ==0:
         screen.blit(arrowl, (50, 250)) # 좌표에 왼쪽 화살표 불러오기
     if yes_no >= 1: # 변수가 1이상 일 때
-        screen.blit(yes_button, ( 515.5 + (album_h / 2) - ( yes_button_h *2)    , 277.5 + (album_w/3))) # 예 버튼 불러오기
-        screen.blit(no_button, ( 515.5 + (album_h / 2) + ( yes_button_h /2)    , 277.5 + (album_w/3) )) # 아니오 버튼 불러오기
+        screen.blit(yesno, (277.5, 208.5)) # 예 버튼 불러오기
+        screen.blit(yes_button, (537, 358.5)) # 예 버튼 불러오기
+        screen.blit(no_button, (662, 358.5)) # 아니오 버튼 불러오기
+    if fairy_condition >=5:
+        screen.blit(fairy, (800, 400)) # 요정 불러오기
     if game >=1: # 변수가 1이상 일 때
+        screen.blit(background_opa, (0, 0)) # 0, 0 좌표에 배경 불러오기
         screen.blit(game_bar, (305, 605)) # 게임 바 불러오기
         screen.blit(game_bar, (445, 605)) # 게임 바 불러오기
         screen.blit(game_bar, (585, 605)) # 게임 바 불러오기
@@ -318,9 +320,6 @@ while running: # True 시 진행 False 시 종료
         if no_pull == 5:  # 버튼을 눌러 no_pull가 5일떄에
             screen.blit(crash_square, (860,0))  # 좌표에 그림 출력
 
-        timer = game_font.render(str(int(elapsed_time1)), True, (255, 255, 255)) # 시간 표시 폰트 이미지 불러오기
-        screen.blit(timer, (0, 0)) # 게임 중 시간 표시
-
         score = game_font.render(str("score : "+ str(game_score)), True, (255, 255, 255))
         screen.blit(score, (985, 0))
         if game_down_speed == 10:
@@ -329,21 +328,26 @@ while running: # True 시 진행 False 시 종료
             elapsed_time = elapsed_time2
         if game_down_speed == 20:
             elapsed_time = elapsed_time3
-        if note_one == 1:                       
-            for i in range(100):
-                real_game_down(i)
-            current_time = int(elapsed_time)
-            note_one = 0
-        if int(elapsed_time) == current_time:
-            pass
-        else:
-            note_one = 1
+        if int(elapsed_time) == 8:
+            game_play = 1
+        if game_play == 1:
+            if note_one == 1:                       
+                for i in range(78):
+                    real_game_down(i)
+                current_time = int(elapsed_time)
+                note_one = 0
+            if int(elapsed_time) == current_time:
+                pass
+            else:
+                note_one = 1
 
         if screen_w - back_button_w <= mousepos[0] <= screen_w and 0 <=mousepos[1] <= back_button_h: #뒤로가기 버튼 클릭했을때
             game = 0 # 게임 변수를 0으로 정의
             game_score = 0
             pygame.mixer.music.stop() #노래 끄기
             yes_no = 0
+            album_opacity = 0
+            game_play = 0
 
         for game_down_x_pos, game_down_y_pos in game_downs:
             screen.blit(game_down, (game_down_x_pos[0], game_down_y_pos)) # 떨어지는 바 불러오기
